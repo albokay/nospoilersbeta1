ffa4c3949a14e0b6df299ee89f82ebeeef37cc57
albokay <akamalizad@gmail.com>
2026-03-31 12:31:28 -0700

    Improve formatting of future feature alert text
    Updated the future feature text to include line breaks for better formatting.

--- src/extensions/ExtensionDock.tsx
+++ src/extensions/ExtensionDock.tsx
@@ -88,7 +88,7 @@ const hasReachedFriendPost =
         }}
       >
         <div style={{ fontSize: 12, lineHeight: 1.5, fontWeight: 400, color: "#fff" }}>
-          ⏳<b>FUTURE FEATURE:</b> alerts about friends you’re watching shows with. Have they caught up to you? Are they ahead? Are there posts or replies about shows you’re watching together?
+          ⏳<b>FUTURE FEATURE⏳:</b> alerts about friends you’re watching shows with. <br></br><br></br>Have they caught up to you? Are they ahead? Are there posts or replies about shows you’re watching together?
         </div>
       </div>
     </div>
