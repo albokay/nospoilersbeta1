990ea63a22ad6f67bfad2351785352e3c0bb5244
albokay <akamalizad@gmail.com>
2026-04-05 14:07:12 -0700

    Shrink progress select font to 12px to fix clipped descenders

--- src/components/OneSelectProgress.tsx
+++ src/components/OneSelectProgress.tsx
@@ -116,7 +116,7 @@ export default function OneSelectProgress({
           className="badge h40"
           value={selectedId}
           onChange={onSelect}
-          style={{ background: "#bdd4de", color: "#2256c9", border: "3px solid #bdd4de", fontWeight: 700 }}
+          style={{ background: "#bdd4de", color: "#2256c9", border: "3px solid #bdd4de", fontWeight: 700, fontSize: 12 }}
         >
           {opts.map((o) => (
             <option key={o.id} value={o.id}>
