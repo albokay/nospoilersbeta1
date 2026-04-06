6153dce4de4e2243b730cc01f865381d0592a3c5
albokay <akamalizad@gmail.com>
2026-04-05 23:57:36 -0700

    Public profile: drop Responses section by 100px

--- src/components/PublicProfilePage.tsx
+++ src/components/PublicProfilePage.tsx
@@ -200,7 +200,7 @@ export default function PublicProfilePage({
               </section>
 
               {/* Their responses */}
-              <section style={{ marginTop: 24 }}>
+              <section style={{ marginTop: 124 }}>
                 <div className="title" style={{ fontSize: 18, marginBottom: 8 }}>Responses</div>
                 <div className="card" style={{ maxHeight: 400, overflowY: "auto" }}>
                   {tabReplies.length === 0 && (
